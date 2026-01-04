87111c732d3bbcbf5592cb5616eeb6aed9e91103
Julius Aako <aakojuiusoluwanifemi@gmail.com>
2026-01-04 14:33:33 +0100

    little ajustment made

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export default function App() {
   ];
 
   const CONTACTS = {
-    whatsapp: 'https://wa.me/2348037200529?text=Hi%20Dev%20AJ%20there!',
+    whatsapp: 'https://wa.me/2349019445903?text=Hi%20Dev%20AJ%20there!',
     facebook: 'https://facebook.com/YOURPROFILE',
     linkedin: 'https://www.linkedin.com/in/YOURPROFILE',
     email: 'mailto:aakojuliusoluwanifemi@gmail.com'
